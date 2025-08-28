4af0e6e8f9e33ad52bd7f794966bb072f4a4e28d
Akarapol Kasivittayanun <akarapol@gmail.com>
2025-08-28 13:03:33 +0700

    Update installer.sh
    fix nginx.conf not exist

--- installer.sh
+++ installer.sh
@@ -201,8 +201,7 @@ enable_production_mode() {
   cd "$instance_path" &&
   sudo pip install ansible --break-system-package &&
   sudo apt install supervisor &&
-  bench setup supervisor &&
-  bench setup nginx &&
+  sudo bench setup production ${USER}
   sudo sed -i 's/user www-data;/user barista;/' /etc/nginx/nginx.conf &&
   sudo bench setup production ${USER}
 }
